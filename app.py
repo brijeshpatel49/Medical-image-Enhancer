48a0d6d4d44482394ca216990dcc2cb1d7ed3075
Brijesh <brijeshp3349@gmail.com>
2025-04-19 13:27:10 +0530

    updated app.py

--- app.py
+++ app.py
@@ -85,6 +85,6 @@ def enhance():
 def home():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
-    app.run(host="0.0.0.0", port=port, debug=True)
+@app.route('/')
+def home():
+    return render_template('index.html')
